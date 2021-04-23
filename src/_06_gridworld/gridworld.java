8c0dfe050079e14facefdc37f873ed68cbec6961
league <league@imac-24.attlocal.net>
2021-04-23 16:56:17 -0700

    CowTimer

--- src/_06_gridworld/gridworld.java
+++ src/_06_gridworld/gridworld.java
@@ -1,5 +1,10 @@
 package _06_gridworld;
 
+import java.awt.Color;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 import info.gridworld.actor.Bug;
 import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
@@ -7,16 +12,40 @@ import info.gridworld.world.World;
 
 public class gridworld {
 	public static void main(String[] args) {
-
+	Random random = new Random();
 	World world = new World();
 	
-	for(int x = 0; x<10; x++) {
-		for(int y = 0; y<10; y++) {
-			Location location = new Location(x, y);
-			Flower flower = new Flower();
-			world.add(location, flower);
-		}
-	}
+//	for(int x = 0; x<10; x++) {
+//		for(int y = 0; y<10; y++) {
+//			Location location = new Location(x, y);
+//			Flower flower = new Flower();
+//			world.add(location, flower);
+//		}
+//	}
+	
+//	Location location = new Location(random.nextInt(10), random.nextInt(10));
+//	Bug bug = new Bug();
+//	world.add(location, bug);
+//	
+	int x1 = random.nextInt(10);
+	int y1 = random.nextInt(10);
+	Location location2 = new Location(x1, y1);
+	Bug bug2 = new Bug();
+	world.add(location2, bug2);
+	bug2.setColor(Color.blue);
+	bug2.turn();
+	bug2.turn();
+	
+	
+	Flower flower = new Flower();
+	Location locationA1 = new Location(x1, y1-1);
+	world.add(locationA1, flower);
+	
+	Flower flower2 = new Flower();
+	Location locationA2 = new Location(x1, y1+1);
+	world.add(locationA2, flower2);
+	
+	
 	world.show();
 	}
 }
--- src/_07_cow_timer/CowTimerRunner.java
+++ src/_07_cow_timer/CowTimerRunner.java
@@ -1,9 +1,16 @@
 package _07_cow_timer;
 
-public class CowTimerRunner {
-	public static void main(String[] args) throws InterruptedException {
-		/* Make a CowTimer, set its time and start it.
-		 * Use a short delay (seconds) when testing, then try with longer delays */
+import javax.swing.SwingUtilities;
 
+import _03_jukebox.Jukebox;
+
+public class CowTimerRunner {
+	public static void main(String[] args) {
+		
+		new CowTimer().start();
 	}
 }
+		/* Make a CowTimer, set its time and start it.
+		 * Use a short delay (seconds) when testing, then try with longer delays */
+		
+	
